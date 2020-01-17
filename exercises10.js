4e705a766f955ac37f70aef9c38be1662f455d96
stevensetiawan94 <steven.setiawan.94@gmail.com>
2020-01-18 02:42:33 +0700

    added

--- exercises10.js
+++ exercises10.js
@@ -1,22 +1,32 @@
 function changeMe(arr) {
-    var hasil={}
-  // you can only write your code here!
-  for(i=1;i<=arr.length;i++){
-      for(j=0;j<arr[i].length;i++){
-        return [arr[j]+arr[j+1]]={'first name':arr[j],'lastName':arr[j+1],'gender':arr[j+2],'age':arr[j+3]}   
-      }
-  }
+    if (arr[0] === undefined) {
+        return ""
+    }
+    let obj = {}
+    for (let i = 0; i < arr.length; i++) {
+        console.log((i + 1) + '. ' + arr[i][0] + ' ' + arr[i][1] + ':');
+        obj['firstName'] = arr[i][0]
+        obj['lastName'] = arr[i][1]
+        obj['gender'] = arr[i][2]
+        if (!arr[i][3] || arr[i][3] > 2019) {
+            obj['age'] = 'Invalid Birth Year'
+        } else {
+            obj['age'] = 2019 - arr[i][3]
+        }
+        console.log(obj)
+    }
 }
-
-// TEST CASES
-changeMe([['Christ', 'Evans', 'Male', 1982], ['Robert', 'Downey', 'Male']]); // 1. Christ Evans:
-// { firstName: 'Christ',
-//   lastName: 'Evans',
-//   gender: 'Male',
-//   age: 37 }
-// 2. Robert Downey:
-// { firstName: 'Robert',
-//   lastName: 'Downey',
-//   gender: 'Male',
-//   age: 'Invalid Birth Year' }
-changeMe([]); // ""
+  
+  // TEST CASES
+  changeMe([['Christ', 'Evans', 'Male', 1982], ['Robert', 'Downey', 'Male']]); 
+  // 1. Christ Evans:
+  // { firstName: 'Christ',
+  //   lastName: 'Evans',
+  //   gender: 'Male',
+  //   age: 37 }
+  // 2. Robert Downey:
+  // { firstName: 'Robert',
+  //   lastName: 'Downey',
+  //   gender: 'Male',
+  //   age: 'Invalid Birth Year' }
+  changeMe([]); // ""
